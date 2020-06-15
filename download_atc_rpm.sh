a3a047a3eb89400f27ef4d4e779e0efe93767733
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2020-06-15 20:19:40 +0200

    Added latest as tag for version

--- download_atc_rpm.sh
+++ download_atc_rpm.sh
@@ -3,10 +3,10 @@
 # Helper script that downloads ATC RPM components.
 #
 
-DO_BASE="https://api.github.com/repos/F5Networks/f5-declarative-onboarding/releases/tags/v"
-AS3_BASE="https://api.github.com/repos/F5Networks/f5-appsvcs-extension/releases/tags/v"
-TS_BASE="https://api.github.com/repos/F5Networks/f5-telemetry-streaming/releases/tags/v"
-CFE_BASE="https://api.github.com/repos/F5Networks/f5-cloud-failover-extension/releases/tags/v"
+DO_BASE="https://api.github.com/repos/F5Networks/f5-declarative-onboarding/releases/"
+AS3_BASE="https://api.github.com/repos/F5Networks/f5-appsvcs-extension/releases/"
+TS_BASE="https://api.github.com/repos/F5Networks/f5-telemetry-streaming/releases/"
+CFE_BASE="https://api.github.com/repos/F5Networks/f5-cloud-failover-extension/releases/"
 
 VALID_COMPONENTS="do DO as3 AS3 ts TS cfe CFE"
 
@@ -17,7 +17,7 @@ function printhelp {
   echo "        AS3/as3 : Application Services 3"
   echo "        TS/ts   : Telemetry Streaming"
   echo "        CFE/cfe : Cloud Failover Extension"
-  echo "    <component_version> the version like a.b.c (with a b and c numbers only)"
+  echo "    <component_version> the version like a.b.c (with a b and c numbers only) or 'latest'"
   echo "    <output_file> absolute output file path in a pre-existing folder"
   exit 0
 }
@@ -34,8 +34,8 @@ if ! [[ ${VALID_COMPONENTS} == *${1}* ]]; then
 fi
 
 # Check if the version matches expected format
-if ! [[ "${2}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then 
-  echo "Wrong version value '${2}', valid format is a.b.c (a b c are numbers only)"
+if ! [[ "${2}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ || "${2}" == "latest" ]]; then 
+  echo "Wrong version value '${2}', valid format is a.b.c (a b c are numbers only) or 'latest'"
   printhelp
 fi
 
@@ -60,16 +60,22 @@ if [[ -z ${OUTPUT_FILE} ]] ; then
   printhelp
 fi
 
+if [ "${2}" = "latest" ] ; then
+  VERSION_PREPEND=""
+else
+  VERSION_PREPEND="tags/v"
+fi
+ 
 # Compose the download URL
 case ${1} in 
   DO | do)
-    URL="${DO_BASE}${2}" ;;
+    URL="${DO_BASE}${VERSION_PREPEND}${2}" ;;
   AS3 | as3)
-    URL="${AS3_BASE}${2}" ;;
+    URL="${AS3_BASE}${VERSION_PREPEND}${2}" ;;
   TS | ts)
-    URL="${TS_BASE}${2}" ;;
+    URL="${TS_BASE}${VERSION_PREPEND}${2}" ;;
   CFE | cfe)
-    URL="${CFE_BASE}${2}" ;;
+    URL="${CFE_BASE}${VERSION_PREPEND}${2}" ;;
   *)
     printhelp ;;
 esac
